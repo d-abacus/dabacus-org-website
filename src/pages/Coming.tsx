dd868e8fd18dbdfa4c2b402a304da005eb29648c
ShuaJJ <Sody008@gmail.com>
2021-04-23 23:44:24 +0800

    added video for coming soon

--- src/pages/Coming.tsx
+++ src/pages/Coming.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { PageContainer } from '@ant-design/pro-layout';
 import styles from './Coming.less';
 
 
 export default (): React.ReactNode => {
 
   return (
-    <PageContainer>
-      <div className={styles.title}>
-        About
-      </div>
-
-
-    </PageContainer>
+    <div className={styles.comingSoon}>
+      <video autoPlay loop muted className={styles.video}>
+        <source src="http://www.sample-videos.com/video123/mp4/360/big_buck_bunny_360p_1mb.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
   );
 };
