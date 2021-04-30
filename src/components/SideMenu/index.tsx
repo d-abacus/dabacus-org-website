c24f0abe67ebbb5900e57e37c40099460b52b602
ShuaJJ <Sody008@gmail.com>
2021-04-30 15:19:09 +0800

    fixed #50

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Menu } from 'antd';
 import { Link, withRouter } from 'umi';
 import indexIcon from '../../assets/index-icon.png';
+import indexLightIcon from '../../assets/index-light-icon.png';
 import farmIcon from '../../assets/farm-icon.png';
 import swapIcon from '../../assets/swap-icon.png';
+import swapLightIcon from '../../assets/swap-light-icon.png';
+import farmLightIcon from '../../assets/farm-light-icon.png';
 import indexActiveIcon from '../../assets/index-icon-active.png';
 import swapActiveIcon from '../../assets/swap-icon-active.png';
 import logo from '../../assets/logo.svg';
@@ -28,14 +31,17 @@ const SideMenu: React.FC<SideMenuProps> = (props: SideMenuProps) => {
         mode="inline"
       >
         <Menu.Item key="index"><Link to="/app/index">
+          <img className="light-icon" src={indexLightIcon} />
           <img className="normal-icon" src={indexIcon} />
           <img className="active-icon" src={indexActiveIcon} /> Index
         </Link></Menu.Item>
         <Menu.Item key="swap"><Link to="/app/swap">
+          <img className="light-icon" src={swapLightIcon} />
           <img className="normal-icon" src={swapIcon} />
           <img className="active-icon" src={swapActiveIcon} /> Swap
         </Link></Menu.Item>
         <Menu.Item key="farm"><Link to="/coming-soon">
+          <img className="light-icon" src={farmLightIcon} />
           <img className="normal-icon" src={farmIcon} /> Farm
         </Link></Menu.Item>
       </Menu>
