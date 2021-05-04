f9863f29964799508be8cd98865c44e28c253c3d
ShuaJJ <Sody008@gmail.com>
2021-05-04 18:53:01 +0800

    added hourly data for index chart

--- src/pages/Dindex.tsx
+++ src/pages/Dindex.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import moment from 'moment';
 import { Table } from 'antd';
 import { Line } from '@ant-design/charts';
 import { PageContainer } from '@ant-design/pro-layout';
@@ -15,8 +16,8 @@ export default (): React.ReactNode => {
   const columns: Array<Object> = [
     {
       title: 'Rank',
-      dataIndex: 'market_cap_rank',
       key: 'rank',
+      render: (text, record, index) => <div>{index+1}</div>,
     },
     {
       title: '',
@@ -55,20 +56,41 @@ export default (): React.ReactNode => {
     asyncFetchCoinData();
   }, []);
   const asyncFetch = () => {
-    fetch('https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json')
+    fetch('https://htapi.easy-ins.cn/1.1/classes/HourlyData?limit=24', {
+      headers: {
+        'X-LC-Id': 'Hg1dKKWBU8CPrnI2M13FSjyT-gzGzoHsz',
+        'X-LC-Key': 'tRpxTTU9l9QROGBKW54h8TcC',
+        'Content-Type': 'application/json'
+      },
+    })
       .then((response) => response.json())
-      .then((json) => setData(json))
+      .then((json) => setData(json['results'].map((obj) => {
+        console.log('ttttttt: ', obj.time);
+        const res = { value: obj.value, "time": moment(obj.time.iso).format('HH')};
+        console.log(JSON.stringify(res));
+        return res;
+      })))
       .catch((error) => {
         console.log('fetch data failed', error);
       });
   };
-  const isBigEnough = (element: Object, index: number, array: Array<Object>) => { 
-   return element["id"] !== 'tether' && element["id"] !== 'usd-coin' && element["id"] !== 'wrapped-bitcoin' && 
-            element["id"] !== 'binance-usd' && element["id"] !== 'cusdc'; 
-  } 
+  const filterTable = (json: Array<Object>) => {
+    var cap: number = 18697762;
+    for (var i=0; i<json.length; i++) {
+      if (json[i]["id"] == "bitcoin") {
+        cap = json[i]["market_cap"];
+        break;
+      }
+    }
+    const factor: number = cap / 243;
+    const resJson = json.filter((element: Object, index: number, array: Array<Object>) => { 
+     return element["id"] !== 'tether' && element["id"] !== 'usd-coin' && element["id"] !== 'wrapped-bitcoin' && 
+              element["id"] !== 'binance-usd' && element["id"] !== 'compound-usd-coin' && element["market_cap"] > factor; 
+    });
+    return resJson;
+  }
   const calTotal = () => {
     var total: number = 0;
-    console.log(coinData);
     for (var i=0;i<tableData.length;i++) {
       const coinId: Object = coinData[tableData[i]["id"]];
       if (coinId) {
@@ -86,9 +108,9 @@ export default (): React.ReactNode => {
       });
   };
   const asyncFetchTable = () => {
-    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=btc&order=market_cap_desc&per_page=23&page=1&sparkline=false')
+    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=btc&order=market_cap_desc&per_page=70&page=1&sparkline=false')
       .then((response) => response.json())
-      .then((json) => setTableData(json.filter(isBigEnough)))
+      .then((json) => setTableData(filterTable(json)))
       .catch((error) => {
         console.log('fetch data failed', error);
       });
@@ -106,19 +128,19 @@ export default (): React.ReactNode => {
   var config = {
     data: data,
     smooth: true,
-    xField: 'Date',
-    yField: 'scales',
+    xField: 'time',
+    yField: 'value',
     color: '#5956FF',
     lineStyle: {
       lineWidth: 2,
     },
-    tooltip: {
-      customContent: (title, data) => {
-        return <div style={{ color: '#AAB0B8', fontWeight: 800, fontSize: 14, fontFamily: 'Avenir-Medium' }}>
-          {title}
-        </div>
-      }
-    },
+    // tooltip: {
+    //   customContent: (title, data) => {
+    //     return <div style={{ color: '#AAB0B8', fontWeight: 800, fontSize: 14, fontFamily: 'Avenir-Medium' }}>
+    //       {JSON.stringify(data.data)}
+    //     </div>
+    //   }
+    // },
     xAxis: { 
       label: labelConfig,
       tickLine: null,
@@ -127,6 +149,8 @@ export default (): React.ReactNode => {
     yAxis: { 
       label: labelConfig,
       tickLine: null,
+      min: 0.000056,
+      max: 0.000058,
       subTickLine: null,
       grid: {
         line: {
@@ -138,17 +162,15 @@ export default (): React.ReactNode => {
     },
   };
   var total: number = tableData.length > 0 ? calTotal() : 0;
-  console.log("total: " + total);
   const currentWorldPopulation: number = 7673533972;
   const averageLifeExpectancyInYears: number = 72.6;
   var WUNBTC: number = total / (currentWorldPopulation * averageLifeExpectancyInYears);
-  console.log("WUNBTC: " + WUNBTC);
   var resData: Array<Object> = tableData.map((element: Object) => {
     return {
       market_cap_rank: element["market_cap_rank"],
       image: element["image"],
       name: element["name"],
-      current_price: (element["current_price"] / WUNBTC).toFixed(2).toLocaleString(),
+      current_price: (element["current_price"] / WUNBTC).toFixed(3).toLocaleString(),
       total_volume: (element["total_volume"] / WUNBTC).toFixed(0).toLocaleString(),
       price_change_percentage_24h: ((element["price_change_24h"] / WUNBTC / (element["current_price"] / WUNBTC) *100).toFixed(2)) + '%',
     }
