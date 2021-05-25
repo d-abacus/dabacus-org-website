c631d8ece54f1e950a57d9bb5b2b831234dbac9f
ShuaJJ <Sody008@gmail.com>
2021-05-25 15:07:38 +0800

    added drizzle

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -5,11 +5,14 @@ import { connect, Link } from 'umi';
 import { Row, Col, Menu, Affix } from 'antd';
 import type { ConnectState } from '@/models/connect';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { DrizzleContext } from '@drizzle/react-plugin'
 import Header from '../components/Header';
 import SideMenu from '../components/SideMenu';
 import TabBar from '../components/TabBar';
 import styles from './AppLayout.less';
 import '../components/AppHeader/menu.less';
+import JOJOToken from '../contracts/JOJOToken.json';
+import { Drizzle } from "@drizzle/store";
 
 
 type MainLayoutProps = {
@@ -36,6 +39,19 @@ class MainLayout extends React.Component<MainLayoutProps, MainLayoutState> {
   render() {
     const { isReady, top } = this.state;
     const { children, loading } = this.props;
+    const drizzleOptions = {
+      web3: {
+        block: false,
+        fallback: {
+          type: "ws",
+          url: "ws://127.0.0.1:7545"
+        }
+      },
+      contracts: [
+       JOJOToken
+      ],
+    };
+    const drizzle = new Drizzle(drizzleOptions);
 
     if (loading || !isReady) {
       return <PageLoading />;
@@ -47,20 +63,22 @@ class MainLayout extends React.Component<MainLayoutProps, MainLayoutState> {
           <meta name="description" content="DABACUS" />
         </Helmet>
 
+        <DrizzleContext.Provider drizzle={drizzle}>
         <div className={styles.container}>
           <SideMenu />
           <div className={styles.pageContentContainer}>
-          <Affix offsetTop={top} className={styles.appHeader + " app-header"}>
-            <Header />
-          </Affix>
-          <div className={styles.pageContentWrapper}>
-          { children }
-          </div>
-          <div className={styles.tabBar}>
-            <TabBar />
-          </div>
+            <Affix offsetTop={top} className={styles.appHeader + " app-header"}>
+              <Header />
+            </Affix>
+            <div className={styles.pageContentWrapper}>
+            { children }
+            </div>
+            <div className={styles.tabBar}>
+              <TabBar />
+            </div>
           </div>
         </div>
+        </DrizzleContext.Provider>
       </HelmetProvider>
     );
   }
