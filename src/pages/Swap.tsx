973f98f2a6b9618b1ec42fc03237e23c8c898111
ShuaJJ <Sody008@gmail.com>
2021-07-21 21:57:41 +0800

    implemented swap

--- src/pages/Swap.tsx
+++ src/pages/Swap.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { connect } from 'umi';
-import { Input, Select, Alert } from 'antd';
+import { Input, Select, notification } from 'antd';
 const { Option } = Select;
 import { PageContainer } from '@ant-design/pro-layout';
+import { LoadingOutlined } from '@ant-design/icons';
 import TokenSwapContract from "../contracts/TokenSwap.json";
-import YARATokenContract from "../contracts/YARAToken.json";
+import ERC20_ABI from "../contracts/erc20.json"
+import RINKEBY_TOKEN_LIST from "../contracts/rinkeby-token-list.json"
 import type { GlobalModelState } from '@/models/global';
 import type { ConnectState } from '@/models/connect';
 import type { Dispatch } from 'umi';
@@ -21,33 +23,41 @@ const SwapPage: React.FC<SwapProps> = (props: SwapProps) => {
 
   const { global } = props;
   const { web3, address } = global;
-  const [selectedToken, setSelectedToken] = useState('eth');
+  const [selectedToken, setSelectedToken] = useState(0);
+  const [loading, setLoading] = useState(false);
   const [selectedAmount, setSelectedAmount] = useState(0);
   const [transferAmount, setTransferAmount] = useState(0);
-  const [showInfo, setShowInfo] = useState("");
 
-  const handleChange = (obj) => {
-    setSelectedToken(obj);
-    setTransferAmount(obj === 'eth' ? selectedAmount * 3000 : selectedAmount);
+  const openNotification = (type: string, msg: string) => {
+    notification[type]({
+      message: msg,
+      description:
+        '',
+    });
+  };
+
+  const handleChange = (obj: string) => {
+    setSelectedToken(parseInt(obj));
+    setTransferAmount(obj === '0' ? selectedAmount * 30000 : selectedAmount * 200);
   }
 
   const handleAmountChange = (e) => {
     const { value } = e.target;
     setSelectedAmount(value);
-    setTransferAmount(selectedToken === 'eth' ? value * 3000 : value);
+    setTransferAmount(selectedToken == 0 ? value * 30000 : value * 200);
   }
 
   const menu = (
     <Select 
-      value={selectedToken} 
+      value={selectedToken.toString()} 
       style={{ minWidth: 120 }}
       onChange={handleChange} 
       dropdownClassName="token-select"
       dropdownMatchSelectWidth={300}
     >
-      <Option value="eth">ETH</Option>
-      <Option value="usdt">USDT</Option>
-      <Option value="uni">UNI</Option>
+      {RINKEBY_TOKEN_LIST.map((token, index) => 
+        <Option key={index} value={index.toString()}>{token.symbol} - {token.name}</Option>
+      )}
     </Select>
   );
 
@@ -60,43 +70,54 @@ const SwapPage: React.FC<SwapProps> = (props: SwapProps) => {
   }
 
   const swap = async () => {
-    if (web3 && address) {
+    if (web3 && address && !loading) {
+      setLoading(true);
 
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = TokenSwapContract.networks[networkId];
       const swapAddress = deployedNetwork.address;
-      const tokenAddress = deployedNetwork.address;
+      const tokenAddress = RINKEBY_TOKEN_LIST[selectedToken].address;
       const contract = new web3.eth.Contract(
         TokenSwapContract.abi,
         swapAddress,
       );
 
-      const deployedTokenNetwork = YARATokenContract.networks[networkId];
-      const tokenContract = new web3.eth.Contract(
-        YARATokenContract.abi,
-        tokenAddress,
-      );
-
-      if (selectedToken === 'eth') {
+      if (selectedToken == 0) {
         contract.methods.swapETH().send({ from: address, value: web3.utils.toWei(selectedAmount.toString(), "ether") }, function(error, transactionHash){ 
+          setLoading(false);
           if (error) {
-            console.log(error + '!!!!');
+            openNotification('error', error);
           } else {
-            console.log(transactionHash + '......');
+            openNotification('success', "Transaction sent");
           }
         });
       } else {
+
+        const tokenContract = new web3.eth.Contract(
+          ERC20_ABI,
+          tokenAddress,
+        );
+  
+        const balance = await tokenContract.methods.balanceOf(address).call({from: address});
+        console.log(balance);
+        if (parseFloat(web3.utils.fromWei(balance)) < selectedAmount) {
+          openNotification('error', "You do not have enough balance to swap");
+          setLoading(false);
+          return;
+        }
+
         tokenContract.methods.approve(swapAddress, web3.utils.toWei(selectedAmount.toString(), "ether")).send({ from: address }, function(error, transactionHash){ 
           if (error) {
-            console.log(error + '!!!!');
+            setLoading(false);
+            openNotification('error', error);
           } else {
-            console.log(transactionHash + '......');
   
             contract.methods.swapOtherTokens(tokenAddress, selectedAmount).send({ from: address }, function(error, transactionHash){ 
+              setLoading(false);
               if (error) {
-                console.log(error + '???????');
+                openNotification('error', error);
               } else {
-                console.log(transactionHash + ',,,,,,,,');
+                openNotification('success', "Transaction sent");
               }
             });
           }
@@ -112,8 +133,6 @@ const SwapPage: React.FC<SwapProps> = (props: SwapProps) => {
     <div className="swap-page">
       <div className="swap-wrapper">
         <div className="swap-title">Swap</div>
-        {showInfo === 'success' && <Alert message="Success Tips" type="success" showIcon />}
-        {showInfo !== 'success' && showInfo.length > 0 && <Alert message="Error" type="error" showIcon />}
         <div className="swap-subtitle">From</div>
           <Input
             type="number"
@@ -127,10 +146,10 @@ const SwapPage: React.FC<SwapProps> = (props: SwapProps) => {
             className="swap-input"
             placeholder="0.0"
             disabled={true}
-            addonAfter="SLG"
+            addonAfter="dAbacus"
             value={transferAmount}
           />
-          <div className="swap-button" onClick={() => swap()}>Swap</div> 
+          <div className="swap-button" onClick={() => swap()}>{loading ? (<LoadingOutlined />) : (<span>Swap</span>)}</div> 
       </div>
     </div>
   </PageContainer>;
